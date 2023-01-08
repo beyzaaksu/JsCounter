c4ae3f64cf386543af28a729cea80cfbf51f005a
beyzaaksu <79789978+beyzaaksu@users.noreply.github.com>
2023-01-08 15:23:19 +0300

    localStorage eklendi

--- functions-bolum-sonu-egzersizi.js
+++ functions-bolum-sonu-egzersizi.js
@@ -1,5 +1,6 @@
 
-let counter=0
+//counter'ı localStorage ile get etmeden önce Number'a çevirdik böylelikle sayaca string olarak eklenmesini engelleyeceğiz
+let counter= localStorage.getItem('counter') ? Number(localStorage.getItem('counter')) : 0 //counter'ın localStorage'da kaydedilen counter key'indeki bilgiden devam etmesini sağlamak için (eğer localStorage'da bilgi yoksa 0 set edecek) 
 let counterDOM= document.querySelector('#counter') //id'si counter olanın DOM'una eriştik
 let increaseDOM= document.querySelector('#increase')  //id'si increase olanın DOM2una eriştik
 let decreaseDOM= document.querySelector('#decrease') //id'si decrease olanın DOM'una eriştik
@@ -16,6 +17,7 @@ decreaseDOM.addEventListener("click", clickEvent ) //id'si decrease olanda click
         counterDOM.innerHTML = counter += 1 // 1 arttır
     }
     else if (this.id=="decrease"){ //eğer id'si decrease olana tıklanırsa
-        counterDOM.innerHTML = counter -= 1 // 1 azalt
+        counterDOM.innerHTML = counter -= 1 // 1 azalt     
     }
+    localStorage.setItem('counter', counter) //local storage içinde counter key'inin içersinde counter value'su tutuluyor
  }
